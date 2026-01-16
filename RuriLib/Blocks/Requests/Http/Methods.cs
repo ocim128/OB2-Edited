9ae0d2dc0cfbb6a0f4582f30be6e93368d92ea69
Mickey <user@DESKTOP-NE8ADSU>
2026-01-16 09:46:03 +0700

    Add TlsClient handler mapping and update Shared package lock

--- RuriLib/Blocks/Requests/Http/Methods.cs
+++ RuriLib/Blocks/Requests/Http/Methods.cs
@@ -41,6 +41,7 @@ namespace RuriLib.Blocks.Requests.Http
                 HttpLibrary.SystemNet => new HttpClientRequestHandler(),
                 HttpLibrary.RestSharp => new RestSharpRequestHandler(),
                 HttpLibrary.WinHttp => new WinHttpClientRequestHandler(),
+                HttpLibrary.TlsClient => new TlsClientRequestHandler(),
                 _ => throw new System.NotImplementedException()
             };
     }
